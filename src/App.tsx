7f4876802aa56e9fd5320a49427128f099f834e9
SNIPBIT\karlo <karloseler@gmail.com>
2026-05-20 19:19:54 +0200

    app update

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,20 @@
-import './App.css';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
-function App() {
-  return 'Hello world';
+import { Layout } from './components/Layout';
+import { Dashboard } from './pages/Dashboard';
+import { ProjectDetail } from './pages/ProjectDetail';
+import { Projects } from './pages/Projects';
+
+export default function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route element={<Layout />}>
+          <Route path="/" element={<Dashboard />} />
+          <Route path="/projects" element={<Projects />} />
+          <Route path="/projects/:id" element={<ProjectDetail />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  );
 }
-
-export default App;
